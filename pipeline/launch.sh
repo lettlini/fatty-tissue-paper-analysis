82a4061972b8d04a46f291ea83304cbc5c49f5e3
Kolya Lettl <klettl@posteo.com>
2025-01-13 13:10:45 +0100

    pipeline: request 12 hours for nextflow head job instead of 7 days

--- pipeline/launch.sh
+++ pipeline/launch.sh
@@ -3,9 +3,9 @@
 #SBATCH --output=/work/kl63sahy-monolayer/nextflow/nextflow-logs/nextflow_%j.out       # Standard output log (%j will be replaced with the job ID)
 #SBATCH --error=/work/kl63sahy-monolayer/nextflow/nextflow-logs/nextflow_%j.err        # Standard error log (%j will be replaced with the job ID)
 #SBATCH --ntasks=1                     # Number of tasks (we're running a single task, Nextflow will handle the rest)
-#SBATCH --cpus-per-task=2             # Number of CPU cores per task
-#SBATCH --mem=5G                     # Memory allocation per task (adjust as needed)
-#SBATCH --time=7-00:00:00
+#SBATCH --cpus-per-task=1             # Number of CPU cores per task
+#SBATCH --mem=2G                     # Memory allocation per task (adjust as needed)
+#SBATCH --time=12:00:00
 #SBATCH --partition=polaris-long           # Partition to submit to (adjust if needed)
 
 OUTPUT_DIR=/work/kl63sahy-monolayer/nextflow/nextflow-reports/${SLURM_JOB_ID}/
